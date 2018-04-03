3a467bcd91ed4a1e39a95537570228ff24c0d001
isengardd <xuyao8598@msn.com>
2018-04-03 22:43:47 +0800

    法宝基础设计上传

---
+++ Assets/Scripts/Battle/Model/Relic/RelicInstance.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// todo 创建工厂类
+
+/// <summary>
+/// 法宝实例
+/// </summary>
+public class RelicHuZangHuFu : RelicBase
+{
+    public RelicHuZangHuFu() : base(RelicId.HUZANG_HUFU)
+    {
+    }
+}
+
+public class RelicWuHuoQiQinShan : RelicBase
+{
+    public RelicWuHuoQiQinShan() : base(RelicId.WUHUOQIQIN_SHAN)
+    {
+    }
+}
+
+public class RelicWuJinHu : RelicBase
+{
+    public RelicWuJinHu() : base(RelicId.WUJIN_HU)
+    {
+    }
+}
+
+public class RelicXueJingShi : RelicBase
+{
+    public RelicXueJingShi() : base(RelicId.XUEJING_SHI)
+    {
+    }
+}
